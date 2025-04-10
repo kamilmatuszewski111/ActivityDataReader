b46789e1a4c5628bcaa22e8ed06ff52cf55356d7
kamilmatuszewski111 <kamilmatuszewski95@gmail.com>
2025-04-10 19:35:30 +0200

    Init for strava file and env file.

--- strava_api.py
+++ strava_api.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 
 TOKEN_URL = "https://www.strava.com/oauth/token"
 ACTIVITIES_URL = "https://www.strava.com/api/v3/athlete/activities"
-AUTHORIZATION_CODE = "d26dc961f305425071a6218b62b7e86afe33d2c7"
+# AUTHORIZATION_CODE = "d26dc961f305425071a6218b62b7e86afe33d2c7"
 
 # response = requests.post(TOKEN_URL, data={
 #     "client_id": loaded_tokens["CLIENT_ID"],
@@ -22,7 +22,10 @@ class StravaAPI:
     def __init__(self):
         self.loaded_tokens = defaultdict()
         self._load_tokens()
-        load_dotenv()
+        self.start_date = None
+        self.end_date = None
+        self.activities = None
+        load_dotenv(override=True)
 
     def _save_to_env(self, data: dict):
         logger.info("Saving data to .env file.")
@@ -34,11 +37,11 @@ class StravaAPI:
         logger.info("Loading tokens from env file.")
         # with open("api_tokens.json", "r", encoding="utf-8") as file:
         #     self.loaded_tokens = json.load(file)
-        self.loaded_tokens["CLIENT_ID"] = os.getenv("CLIENT_ID")
+        self.loaded_tokens["CLIENT_ID"] = int(os.getenv("CLIENT_ID"))
         self.loaded_tokens["CLIENT_SECRET"] = os.getenv("CLIENT_SECRET")
         self.loaded_tokens["ACCESS_TOKEN"] = os.getenv("ACCESS_TOKEN")
         self.loaded_tokens["REFRESH_TOKEN"] = os.getenv("REFRESH_TOKEN")
-        self.loaded_tokens["EXPIRES_AT"] = os.getenv("EXPIRES_AT")
+        self.loaded_tokens["EXPIRES_AT"] = int(os.getenv("EXPIRES_AT"))
         return self.loaded_tokens
 
     @staticmethod
@@ -72,22 +75,26 @@ class StravaAPI:
         if self._is_token_expired(self.loaded_tokens["EXPIRES_AT"]):
             logger.warning("Token is expired. Refresh token required.")
             self.refresh_token()
+            time.sleep(5)
             self._load_tokens()
         else:
             logger.success("Token is not expired. Send API request allowed.")
 
-    def send_request(self):
+    def activities_request(self):
         self.check_token_expiration()
         headers = {"Authorization": f"Bearer {self.loaded_tokens['ACCESS_TOKEN']}"}
 
-        logger.info("Create datetime params.")
+        logger.info("Create datetime params. Please put your start and end date.")
 
         # params = {
         #     'keys': 'heartrate,velocity_smooth',
         #     'key_by_type': 'true',
         # }
-        after = int(time.mktime(time.strptime('2025-01-01', '%Y-%m-%d')))
-        before = int(time.mktime(time.strptime('2025-04-07', '%Y-%m-%d')))
+        self.start_date = input("Start date in YYYY-MM-DD format")
+        self.end_date = input("End date in YYYY-MM-DD format")
+
+        after = int(time.mktime(time.strptime(self.start_date, '%Y-%m-%d')))
+        before = int(time.mktime(time.strptime(self.end_date, '%Y-%m-%d')))
         params = {
             'after': after,
             'before': before,
@@ -96,16 +103,22 @@ class StravaAPI:
 
         logger.info("Sending request...")
         response = requests.get(ACTIVITIES_URL, headers=headers, params=params)
-
         if response.status_code == 200:
             logger.success("Response from server received correctly.")
-            activities = response.json()
-            for activity in activities:
-                print(f"Aktywność: {activity['name']}, Typ: {activity['type']}, Dystans: {activity['distance']} m")
+            self.activities = response.json()
+            for activity in self.activities:
+                print(f"ID: {activity['id']}, "
+                      f"Datetime: {activity['start_date_local']}, "
+                      f"Type: {activity['type']}, "
+                      f"Distance: {activity['distance']/1000} km,")
+            return self.activities
         else:
             logger.error("Response from server received incorrectly.", response.status_code, response.text)
             print("Error:", response.status_code, response.text)
+            return None
 
+    def activities_details_request(self):
+        pass
 
 kupa = StravaAPI()
-kupa.send_request()
+activities = kupa.activities_request()
